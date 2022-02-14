533f4c8ae32b8658398db330abf42281681d45c0
Marc Kohaupt <debuglevel@gmail.com>
2022-02-14 21:37:17 +0100

    Revert accident commit

--- app/library/database.py
+++ app/library/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.sqlite"
-#SQLALCHEMY_DATABASE_URL = "sqlite:////data/database.sqlite"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./database.sqlite"
+SQLALCHEMY_DATABASE_URL = "sqlite:////data/database.sqlite"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
